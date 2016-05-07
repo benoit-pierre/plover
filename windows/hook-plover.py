963cedeb57bf7a5530ee5ff733a6bad5b0e9f9be
Benoit Pierre <benoit.pierre@gmail.com>
2016-05-07 06:40:45 +0200

    [tmp] dist with plugins

--- windows/hook-plover.py
+++ windows/hook-plover.py
@@ -6,6 +6,7 @@ import pkg_resources
 from utils.metadata import collect_metadata
 
 from PyInstaller import log as logging
+from PyInstaller.utils.hooks import eval_statement
 
 
 log = logging.getLogger(__name__)
@@ -17,9 +18,8 @@ hiddenimports = []
 distribution = list(pkg_resources.find_distributions('.', only=True))[0]
 assert distribution.project_name == 'plover'
 
-metadata_list = collect_metadata(distribution)
-log.info('adding metadata: %s', metadata_list)
-datas.extend(metadata_list)
+metadata = {}
+metadata.update(collect_metadata(distribution))
 
 datas.append(('plover/assets', 'plover/assets'))
 for group in distribution.get_entry_map().values():
@@ -38,17 +38,25 @@ if os.path.exists(plugins_dir):
     if errors:
         log.error("error(s) while loading plugins: %s", errors)
 
+    plugins_imports = set()
+
     for distribution in pkg_resources.find_distributions(plugins_dir):
         log.info('adding plugin: %s', distribution.project_name)
         egg = '%s/%s.egg' % (plugins_dir, distribution.egg_name())
         datas.append((egg, 'eggs'))
-        datas.extend(collect_metadata(distribution))
+        metadata.update(collect_metadata(distribution))
         for group_name, group in distribution.get_entry_map().items():
             for entrypoint in group.values():
                 log.info('%s entrypoint: %s [%s]', group_name, entrypoint, entrypoint.module_name)
+                script = '''
+                plugin_path = %r
+                plugins_dir = %r
+                import os, sys
+                sys.path.insert(0, plugin_path)
                 imports = set(sys.modules.keys())
-                entrypoint = entrypoint.load()
+                import %s
                 imports = set(sys.modules.keys()) - imports
+                hiddenimports = []
                 for module_name in imports:
                     if module_name == 'plover' or module_name.startswith('plover.'):
                         continue
@@ -60,6 +68,17 @@ if os.path.exists(plugins_dir):
                         continue
                     if not os.path.realpath(mod.__file__).startswith(plugins_dir):
                         hiddenimports.append(module_name)
+                print hiddenimports
+                ''' % (egg, plugins_dir, entrypoint.module_name)
+                result = eval_statement(script)
+                plugins_imports.update(result)
+
+    log.info('plugins hidden imports: %s', plugins_imports)
+    hiddenimports.extend(plugins_imports)
+
+metadata_list = list(sorted(metadata.items()))
+log.info('adding metadata: %s', metadata_list)
+datas.extend(metadata_list)
 
 log.info('datas: %s', datas)
 log.info('hiddenimports: %s', hiddenimports)
