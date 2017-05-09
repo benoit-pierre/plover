415961611a8b6f569c719bfa61956fd0037d9e5e
Benoit Pierre <benoit.pierre@gmail.com>
2017-05-09 04:17:10 +0200

    tmp

--- plover/orthography.py
+++ plover/orthography.py
@@ -4,46 +4,55 @@
 """Functions that implement some English orthographic rules."""
 
 from plover import system
+from plover.hunspell import load_hunspell_wordmap
 
+WORDMAP = load_hunspell_wordmap('plover/assets/en_US.dic',
+                                'plover/assets/en_US.aff')
 
-def make_candidates_from_rules(word, suffix, check=lambda x: True):
-    candidates = []
+def make_candidates_from_rules(word, suffix):
     for r in system.ORTHOGRAPHY_RULES:
         m = r[0].match(word + " ^ " + suffix)
-        if m:   
-            expanded = m.expand(r[1])
-            if check(expanded):
-                candidates.append(expanded)
-    return candidates
+        if m:
+            yield m.expand(r[1])
 
 def _add_suffix(word, suffix):
-    in_dict_f = lambda x: x in system.ORTHOGRAPHY_WORDS
+
+    # First, check for a valid known derivative.
+    hw = WORDMAP.get(word)
+    if hw is not None:
+        for hw in hw.derivatives:
+            for compo in hw.compositions:
+                if compo[0] or compo[2] != suffix:
+                    continue
+                return hw.word
 
     candidates = []
-    
+
+    # Then try orthographic rules.
+    for c in make_candidates_from_rules(word, suffix):
+        if c in WORDMAP:
+            # If it's a valid word, return it.
+            return c
+        candidates.append(c)
+
+    # If there's an alias, try the rules again.
     alias = system.ORTHOGRAPHY_RULES_ALIASES.get(suffix, None)
     if alias is not None:
-        candidates.extend(make_candidates_from_rules(word, alias, in_dict_f))
-    
+        for c in make_candidates_from_rules(word, alias):
+            if c in WORDMAP:
+                # If it's a valid word, return it.
+                return c
+            candidates.append(c)
+
     # Try a simple join if it is in the dictionary.
     simple = word + suffix
-    if in_dict_f(simple):
-        candidates.append(simple)
-    
-    # Try rules with dict lookup.
-    candidates.extend(make_candidates_from_rules(word, suffix, in_dict_f))
+    if simple in WORDMAP:
+        return simple
 
-    # For all candidates sort by prominence in dictionary and, since sort is
-    # stable, also by the order added to candidates list.
-    if candidates:
-        candidates.sort(key=lambda x: system.ORTHOGRAPHY_WORDS[x])
-        return candidates[0]
-    
-    # Try rules without dict lookup.
-    candidates = make_candidates_from_rules(word, suffix)
+    # Try the first result from the orthographic rules.
     if candidates:
         return candidates[0]
-    
+
     # If all else fails then just do a simple join.
     return simple
 
